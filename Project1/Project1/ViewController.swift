0ed868738d5cce9a816a16d44892d99c62b08fc8
Selen <selenyanar@gmail.com>
2022-06-09 18:12:50 +0300

    Initial commit

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Selen Yanar on 9.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
